5cc8793fa97b918c6239d15cab92294632656601
xzq666 <1192227411@qq.com>
2015-08-12 09:57:14 +0800

    测试

---
+++ xzqtest/xzqtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xzqtest
+//
+//  Created by 许卓权 on 15/8/12.
+//  Copyright (c) 2015年 CCT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xzqtest/xzqtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xzqtest
+//
+//  Created by 许卓权 on 15/8/12.
+//  Copyright (c) 2015年 CCT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
